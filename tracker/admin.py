25f95bc1af7699007edd50c7937c3f04d09699eb
greeshma1376 <greeshma@example.com>
2026-08-12 15:34:54 +0530

    Created Task model

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'due_date', 'is_completed')
+    list_filter = ('is_completed', 'due_date')
+    search_fields = ('title', 'description')
--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    due_date = models.DateField()
+    is_completed = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['due_date', '-id']
+
+    def __str__(self):
+        return self.title
